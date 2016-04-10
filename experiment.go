a1c43e14b854247f4f10f3ad8402c99978e2f8f3
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2016-04-10 12:21:43 +0100

    Experiment: add control/test methods.
    This adds the control and test methods that will be used to run our
    experiment.

--- experiment.go
+++ experiment.go
@@ -1,12 +1,18 @@
 package experiment
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+	"sync"
+)
 
 type (
 	// Experiment is the experiment runner. It contains all the logic on how to run
 	// experiments against controls and for a given number of users.
 	Experiment struct {
-		opts options
+		*sync.Mutex
+		opts       options
+		behaviours map[string]*behaviour
 	}
 )
 
@@ -17,13 +23,37 @@ var (
 // New will create a new Experiment and set it up for later usage. If a new
 // experiment is created without name, an error will be returned.
 func New(options ...Option) (*Experiment, error) {
-	exp := &Experiment{opts: newOptions(options...)}
+	exp := &Experiment{
+		Mutex:      &sync.Mutex{},
+		opts:       newOptions(options...),
+		behaviours: map[string]*behaviour{},
+	}
 
 	if exp.Name() == "" {
 		return nil, NoNameError
 	}
 
 	return exp, nil
+}
+
+// Control sets the control method for this experiment. The control should only
+// be set once and this will return an error if this is not the case.
+func (e *Experiment) Control(b BehaviourFunc) error {
+	return e.Test("control", b)
+}
+
+// Test adds a test case to the exeriment. If a test case with the same name is
+// already used, an error will be returned.
+func (e *Experiment) Test(name string, b BehaviourFunc) error {
+	if _, ok := e.behaviours[name]; ok {
+		return errors.New(fmt.Sprintf("Behaviour `%s` already exists.", name))
+	}
+
+	e.Lock()
+	e.behaviours[name] = newBehaviour(name, b)
+	e.Unlock()
+
+	return nil
 }
 
 func (e *Experiment) Name() string {
--- experiment_test.go
+++ experiment_test.go
@@ -1,22 +1,57 @@
-package experiment_test
+package experiment
 
 import (
 	"testing"
 
-	"github.com/jelmersnoeck/experiment"
+	"golang.org/x/net/context"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func TestExperiment_New_NoName(t *testing.T) {
-	_, err := experiment.New()
+	_, err := New()
 
 	assert.NotNil(t, err, "Experiment without name error")
-	assert.IsType(t, experiment.NoNameError, err, "Experiment without name error type")
+	assert.IsType(t, NoNameError, err, "Experiment without name error type")
 }
 
 func TestExperiment_New(t *testing.T) {
-	exp, err := experiment.New(experiment.Name("experiment-test"))
+	exp, err := New(Name("experiment-test"))
 
 	assert.Nil(t, err)
 	assert.Equal(t, "experiment-test", exp.Name(), "Experiment name from opts")
+}
+
+func TestExperiment_Control(t *testing.T) {
+	exp, _ := New(Name("control-test"))
+	assert.Empty(t, exp.behaviours)
+
+	err := exp.Control(dummyTestFunc)
+	assert.NotEmpty(t, exp.behaviours)
+	assert.Nil(t, err)
+
+	err = exp.Control(dummyTestFunc)
+	assert.NotNil(t, err)
+	assert.Len(t, exp.behaviours, 1)
+}
+
+func TestExperiment_Test(t *testing.T) {
+	exp, _ := New(Name("control-test"))
+	assert.Empty(t, exp.behaviours)
+
+	err := exp.Test("first", dummyTestFunc)
+	assert.Nil(t, err)
+	assert.Len(t, exp.behaviours, 1)
+
+	err = exp.Test("first", dummyTestFunc)
+	assert.NotNil(t, err)
+	assert.Len(t, exp.behaviours, 1)
+
+	err = exp.Test("second", dummyTestFunc)
+	assert.Nil(t, err)
+	assert.Len(t, exp.behaviours, 2)
+}
+
+func dummyTestFunc(ctx context.Context) (interface{}, error) {
+	return nil, nil
 }
