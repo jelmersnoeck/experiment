d584e1d2680cf7aaeab2105bef7f647b97097e4f
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2016-04-11 18:07:05 +0200

    Publisher: correct comment start.

--- publisher.go
+++ publisher.go
@@ -1,7 +1,8 @@
 package experiment
 
-// Publisher is what we'll use to make our results visible. Multiple publishers
-// can be used for a single experiment with each having their own purpose.
+// ResultPublisher is what we'll use to make our results visible. Multiple
+// publishers can be used for a single experiment with each having their own
+// purpose.
 type ResultPublisher interface {
 	Publish(Result)
 }
