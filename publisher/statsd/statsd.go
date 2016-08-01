39e5c88694403757af4dc57c8e589d973b0c2d4f
Jelmer Snoeck <jelmer.snoeck@gmail.com>
2016-08-01 16:06:04 +0100

    Statsd: more metrics.

--- publisher/statsd/statsd.go
+++ publisher/statsd/statsd.go
@@ -29,11 +29,21 @@ func (p *statsdPublisher) Publish(res experiment.Result) {
 	for _, ob := range res.Candidates() {
 		p.publishObservation(ob)
 	}
+
+	p.cl.Count(p.bucketName("mismatches")+".count", len(res.Mismatches()))
 }
 
 func (p *statsdPublisher) publishObservation(ob experiment.Observation) {
+	if ob.Error != nil {
+		p.cl.Increment(p.bucketName(ob.Name) + ".errors.incr")
+	}
+
+	if ob.Panic != nil {
+		p.cl.Increment(p.bucketName(ob.Name) + ".panics.incr")
+	}
+
 	p.cl.Timing(
-		p.bucketName(ob.Name),
+		p.bucketName(ob.Name)+".time",
 		int(ob.Duration/time.Millisecond),
 	)
 }
